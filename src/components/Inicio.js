51090acba6006ebd1901543bc2f58d5a17985c29
Renato Mello <renatogsmello@gmail.com>
2021-10-19 22:04:27 -0300

    inclusão das fontes, icones e pagina de habilidades

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -5,12 +5,12 @@ export default function Inicio() {
 		<>
 			<div id="inicio" className="bg-gradient-principal vh-100">
 				<Container>
-					<h1 class="fs-1 fw-bold text-white">Olá, Bem vindo ao meu Portfolio!</h1>
-					<p class="fs-5 text-white">Sou um Web Developer especializado em front-end.</p>
-					<button class="btn btn-primary btn-lg me-3" type="button">
+					<h1 className="fs-1 fw-bold text-white">Olá, Bem vindo ao meu Portfolio!</h1>
+					<p className="fs-5 text-white">Sou um Web Developer especializado em front-end.</p>
+					<button className="btn btn-primary btn-lg me-3" type="button">
 						De uma olhada no meu trabalho
 					</button>
-					<button class="btn btn-outline-success btn-lg" type="button">
+					<button className="btn btn-outline-success btn-lg" type="button">
 						Me mande uma mensagem
 					</button>
 				</Container>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import App from "./components/App"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./styles/style.min.css"
+import "./fonts/fontawesome-free-5/css/all.min.css"
 
 ReactDOM.render(
 	<React.StrictMode>
